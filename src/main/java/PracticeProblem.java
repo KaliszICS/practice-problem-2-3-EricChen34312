b3a48fbbdf2eccfd0e46f0df8583e2b18fd7bc47
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-03-11 15:42:54 +0000

    Solve the thing

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		String[] arr = { "a", "b", "D", "c", "help", "me", "you", "fool" };
+		System.out.println(Arrays.toString(arr));
 
+		bubbleSortString(arr);
 	}
 
 	public static void q1() {
@@ -22,6 +27,38 @@ public class PracticeProblem {
 
 	public static void q5() {
 		//Write question 5 code here
+	}
+
+	public static void bubbleSortString(String[] strings){
+		//iterate the upper bound of times 
+
+		for(int i = 0; i < strings.length; i++){
+
+			//Single bubble sort iteration
+
+			//has swapped flag
+			boolean swap = false;
+
+			for(int j = 0; j < strings.length-1;j++){
+				
+				//System.out.println("Squah");
+
+				if(strings[j].toLowerCase().compareTo(strings[j+1].toLowerCase()) > 0){
+					//System.out.println("hit");
+					//if current comes later than current+1, swap the two
+					
+					String temp = strings[j+1];
+					strings[j+1] = strings[j];
+					strings[j] = temp;
+					swap = true;
+				}
+			}
+
+			if(!swap){
+					//break loop if never swapped
+					i = strings.length;
+				}
+		}
 	}
 
 }
